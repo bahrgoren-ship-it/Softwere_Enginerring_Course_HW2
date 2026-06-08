004d8fb2b663ec8ba54d1fa440b52e17ff3d75ef
bahrg <bahr.goren@gamil.com>
2026-06-08 23:12:28 +0300

    Refactor Director and Movie: fix encapsulation, equals logic, and Javadocs

--- src/Movie.java
+++ src/Movie.java
@@ -7,35 +7,71 @@ public class Movie {
 
     /**
      * Constructor.
+     *
+     * @param movieName the name of the movie
+     * @param genre the genre of the movie
+     * @param releaseYear the release year of the movie
+     * @param director the director of the movie
      */
 
-    public Movie(String movieName, Genre genre, int releaseYear, String directorName) {
+    public Movie(String movieName, Genre genre, int releaseYear, Director director) {
         this.movieName = movieName;
         this.genre = genre;
         this.releaseYear = releaseYear;
-        this.director = Director.getDirector(directorName);//get a director by name or creates new one
+        this.director = director;
     }
 
     /**
      * Displays movie information.
      */
+
     public void showMovie() {
         System.out.print("Title: " + this.movieName + ", Genre: " + this.genre);
-        System.out.print(", Year: " + this.releaseYear + ", director: ");
-        this.director.show();
+        System.out.print(", Year: " + this.releaseYear + ", director: " + this.director.getName());
         System.out.println(".");
     }
 
     /**
-     * Returns whether true if both movies are the same.
+     * Checks if both movies are the same.
+     * A movie is identified by its name, release year, and director.
+     *
+     * @param movie the movie to compare to
+     * @return true if same and false otherwise
      */
     public boolean equals(Movie movie) {
-        if (this.movieName.equals(movie.movieName) && this.genre == movie.genre) {
-            if(this.releaseYear == movie.releaseYear && this.director.equals(movie.director)){
+        if(movie == null) return false;
+        if (this.movieName.equals(movie.movieName) && this.releaseYear == movie.releaseYear
+                && this.director.equals(movie.director)){
                 return true;
-            }
         }
         return false;
+    }
+
+    /**
+     * Gets the movie's name.
+     *
+     * @return the name of the movie
+     */
+    public String getMovieName() {
+        return this.movieName;
+    }
+
+    /**
+     * Gets the movie's release year.
+     *
+     * @return the release year
+     */
+    public int getReleaseYear() {
+        return this.releaseYear;
+    }
+
+    /**
+     * Gets the movie's director.
+     *
+     * @return the director of the movie
+     */
+    public Director getDirector() {
+        return this.director;
     }
 
 }
